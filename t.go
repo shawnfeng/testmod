f71c06c8e5d29caedbc069a04924bd24e9e03b6c
Shawn Feng <fengguangxiang2015@ipalfish.com>
2018-12-20 16:30:44 +0800

    test

--- t.go
+++ t.go
@@ -3,6 +3,6 @@ package testmod
 import "fmt" 
 
 // Hi returns a friendly greeting
-func Hi(name string, arg string) string {
+func Hi(name string, arg string, arg2 string) string {
 	return fmt.Sprintf("Hi, %s arg:%s", name, arg)
 }
